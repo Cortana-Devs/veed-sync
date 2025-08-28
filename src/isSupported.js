0c1b6565fc13931912a2a692a3b77559e9302b79
Chamath Thiwanka <ict21932@fot.sjp.ac.lk>
2025-08-29 02:06:28 +0530

    Enhance isSupported function to include WebGPU support

--- src/isSupported.js
+++ src/isSupported.js
@@ -8,11 +8,12 @@ const isSupported = () => {
   }
 
   const webGL2Supported = !!gl;
+  const webGPUSupported = typeof navigator !== "undefined" && !!navigator.gpu;
   const audioApiSupported = !!(
     window.AudioContext || window.webkitAudioContext
   );
 
-  return webGL2Supported && audioApiSupported;
+  return (webGL2Supported || webGPUSupported) && audioApiSupported;
 };
 
 export default isSupported;
